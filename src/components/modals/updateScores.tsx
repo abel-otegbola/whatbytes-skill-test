976972be8d0d09b9dc4864a82b4497d3882fd4f7
ABEL OTEGBOLA <abel15655@gmail.com>
2025-01-12 08:44:50 +0100

    feat: create outside click effect for closing modal

--- src/components/modals/updateScores.tsx
+++ src/components/modals/updateScores.tsx
@@ -3,6 +3,7 @@ import Input from "../input/input"
 import Button from "../button/button"
 import { Formik } from "formik"
 import { updateSchema } from "@/schema/updateSchema"
+import { useOutsideClick } from "../../helpers/useClickOutside"
 
 export interface dataProps {
     rank: number,
@@ -12,9 +13,11 @@ export interface dataProps {
 
 export default function UpdateScores({ data, setData, setOpenModal, img }: { data: dataProps, setData: (data: dataProps) => void, setOpenModal: (aug0: boolean) => void, img: ReactElement }) {
 
+    const modalRef = useOutsideClick(setOpenModal, false)
+
     return (
         <div className="fixed top-0 left-0 bg-black/[0.5] w-full h-full flex items-center justify-center z-[100]">
-            <div className="md:w-[600px] w-[90%] bg-white flex p-6 flex-col gap-8 rounded-lg">
+            <div ref={modalRef} className="md:w-[600px] w-[90%] bg-white flex p-6 flex-col gap-8 rounded-lg">
                 <div className="w-full flex justify-between gap-6">
                     <h1 className="font-bold text-[16px]">Update Scores</h1>
                     {img}
---
+++ src/helpers/useClickOutside.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useRef } from 'react';
+
+export const useOutsideClick = (callback: (aug0: boolean) => void, value: boolean) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent | TouchEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        callback(value);
+      }
+    };
+
+    document.addEventListener('mouseup', handleClickOutside);
+    document.addEventListener('touchend', handleClickOutside);
+
+
+    return () => {
+      document.removeEventListener('mouseup', handleClickOutside);
+      document.removeEventListener('touchend', handleClickOutside);
+    };
+  }, [callback, value]);
+
+  return ref;
+};
