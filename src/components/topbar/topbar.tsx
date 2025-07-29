d3f5d5c949b840c4634f13e90817705b4639143a
abel-otegbola <abel15655@gmail.com>
2025-07-29 17:03:13 +0100

    style: topbar bg color to black

--- src/components/topbar/topbar.tsx
+++ src/components/topbar/topbar.tsx
@@ -6,7 +6,7 @@ import Image from "next/image"
 function Topbar() {
 
     return (
-        <div className="sticky top-0 left-0 w-full flex justify-between bg-[#000]/[0.5] backdrop-blur-sm shadow-sm py-2 p-4 border-b border-gray-500/[0.1] z-[52]">
+        <div className="sticky top-0 left-0 w-full flex justify-between bg-[#000]/[0.8] backdrop-blur-sm shadow-sm py-2 p-4 border-b border-gray-500/[0.1] z-[52]">
             <div className="flex items-center gap-2">
                 {/* <LogoIcon className="md:w-[30px] w-[20px]" /> */}
                 <h1 className="text-lg font-bold">Ace tests</h1>
